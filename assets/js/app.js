eb7e7372310296485f876994811f310b9144da35
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-19 10:47:32 -0500

    agregando h1 y hr

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
+window.addEventListener("load", function(){
+  var cuerpo = document.getElementById("body");
 
+/** Agregando h1 y hr **/
+  function AddTitle(frase) {
+    var titulo = document.createElement("h1");
+    cuerpo.appendChild(titulo);
+    titulo.appendChild(document.createTextNode(frase));
+
+    var hr = document.createElement("hr");
+    hr.setAttribute("width","200px");
+    cuerpo.appendChild(hr);
+  };
+  AddTitle("Nuestras coders");
+
+
+
+})
