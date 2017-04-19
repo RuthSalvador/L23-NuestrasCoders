6bca7937a9a0788fa8de319fef68c6763cf0f762
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-19 10:48:33 -0500

    agregando coders

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,31 @@ window.addEventListener("load", function(){
   };
   AddTitle("Nuestras coders");
 
+/** Agregando coders **/
+  var coders = ["arantza burga","elizabeth condori","grecia rayme","janine vega","michelle more","rosario felix","yenny velasquez"];
+  var section = document.createElement("section");
+  cuerpo.appendChild(section);
+
+  function AddCoders(array){
+    array.forEach(function(e){
+
+      var figure = document.createElement("figure");
+      figure.setAttribute("class", "figure-coder");
+      section.appendChild(figure);
+
+      var image = document.createElement("img");
+      image.setAttribute("src","assets/img/"+ e + ".jpg");
+      image.setAttribute("title",e);
+      image.setAttribute("class","image-coder");
+      figure.appendChild(image);
+
+      var figcap = document.createElement("figcaption");
+      figcap.setAttribute("class","name-coder");
+      figure.appendChild(figcap);
+    })
+  };
+  AddCoders(coders);
+
 
 
 })
